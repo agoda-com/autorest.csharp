b8555983d932a23c712d60b53a4f54d01cb5a654
Raj Kumar <Raj.Kumar@agoda.com>
2019-05-02 17:23:31 +0700

    Update generate and package scripts

--- build/generate.ps1
+++ build/generate.ps1
@@ -1,1 +1,2 @@
+autorest --reset
 autorest --use=http://10.120.11.112:8080/microsoft.azure-autorest.csharp-2.3.0.tgz --csharp --input-file=swagger.json --output-folder=agoda.csharp.client.test\Client --namespace=Agoda.Csharp.Client.Test
