9f331a98e750bd69ad45b79679bd50215ab2059e
raj <raj.kumar@agoda.com>
2019-05-09 18:43:26 +0700

    Create build from local folder instead of gz

--- build/build.sh
+++ build/build.sh
@@ -14,12 +14,12 @@ testProject="agoda.csharp.client.test/agoda.csharp.client.test.csproj"
 dotnet sln remove $testProject
 dotnet build
 # This will create a tar file that can be used in conjunction with the autorest generate command to generate clients for running tests
-name=$(npm pack)
-package=${name##*$'\n'}
-echo "Package created $package"
-dotnet sln add $testProject 
+#name=$(npm pack)
+#package=${name##*$'\n'}
+#echo "Package created $package"
+#dotnet sln add $testProject 
 
 # autorest --reset 
-autorest --use=$package --csharp --input-file=..\swagger.json --output-folder=.\agoda.csharp.client.test\Client --namespace=Agoda.Csharp.Client.Test
+autorest --use=. --csharp --input-file=..\swagger.json --output-folder=.\agoda.csharp.client.test\Client --namespace=Agoda.Csharp.Client.Test
 
 dotnet test
