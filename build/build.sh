8209c7b09ee41791465e6d8e264c8f85fc1a14f1
Raj Kumar <Raj.Kumar@agoda.com>
2019-05-09 17:20:26 +0700

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -xe 
+
 echo "Clearing old packages"
 
 npm install
@@ -5,22 +7,19 @@ npm install -g gulp
 npm install -g autorest
 
 currentBranch=$(git rev-parse --abbrev-ref HEAD)
-
 echo "Building branch $currentBranch"
 
-$testProject = "agoda.csharp.client.test\\agoda.csharp.client.test.csproj"
+testProject="agoda.csharp.client.test\\agoda.csharp.client.test.csproj"
 
 dotnet sln remove $testProject
 dotnet build
-c=$(npm pack)
-gz=${c##*$'\n'}
-echo "Package created $gz"
+# This will create a tar file that can be used in conjunction with the autorest generate command to generate clients for running tests
+name=$(npm pack)
+package=${name##*$'\n'}
+echo "Package created $package"
 dotnet sln add $testProject 
 
 # autorest --reset 
-autorest --use=$gz --csharp --input-file=..\swagger.json --output-folder=.\agoda.csharp.client.test\Client --namespace=Agoda.Csharp.Client.Test
+autorest --use=$package --csharp --input-file=..\swagger.json --output-folder=.\agoda.csharp.client.test\Client --namespace=Agoda.Csharp.Client.Test
 
-# This will create a tar file that can be used in conjunction with the autorest generate command to generate clients for running tests
 dotnet test
-# rmrf agoda.csharp.client.test
-# git checkout .
