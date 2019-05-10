de93120d8b93826246d0326984481230ab528fdd
raj <raj.kumar@agoda.com>
2019-05-10 12:00:35 +0700

    Prepending http to url

--- build/build.sh
+++ build/build.sh
@@ -20,6 +20,7 @@ echo "Package created $package"
 dotnet sln add $testProject 
 
 autorest --reset 
-autorest --use=localhost:8085/$package --csharp --input-file=./swagger/swagger.json --output-folder=./agoda.csharp.client.test/Client --namespace=Agoda.Csharp.Client.Test
+autorest --use=http://localhost:8085/$package --csharp --input-file=./swagger/swagger.json --output-folder=./agoda.csharp.client.test/Client --namespace=Agoda.Csharp.Client.Test
 # good agent hk-unix59-19
+# dotnet build
 dotnet test
